9c712dd67201312a17d5a143005308dc6afd160d
李朝良 <lichaoliang@yanxiu.com>
2026-04-13 18:37:11 +0800

    提交一下

--- crontab_task/get_Genshin_social_network.py
+++ crontab_task/get_Genshin_social_network.py
@@ -16,6 +16,7 @@ class GenshinSocialNetwork:
     def __init__(self):
         self.characters = []
         self.social_network = {}
+        self.time_sleep = 3
 
     def get_social_network(self):
         self.step1()
@@ -53,7 +54,7 @@ class GenshinSocialNetwork:
     def step2(self):
         logger.info("开始执行步骤2：获取角色名称英文列表")
         for _ in range(len(self.characters)):
-            time.sleep(1)
+            time.sleep(self.time_sleep)
             self.characters[_]["name_en"] = self.scrpayer_step2(self.characters[_]["name_zn"])
         s=",".join([character['name_en'] for character in self.characters])
         logger.info(f"【原神角色英文名称】：{s}")
@@ -80,7 +81,7 @@ class GenshinSocialNetwork:
     def step3(self):
         logger.info("开始执行步骤3：获取角色社交网络数据")
         for character in self.characters:
-            time.sleep(1)
+            time.sleep(self.time_sleep)
             name_zn = character["name_zn"]
             logger.info(f"开始获取角色 {name_zn} 的社交网络数据")
             self.scrpayer_step3(name_zn)
